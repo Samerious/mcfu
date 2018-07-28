e650c169d7dbc248ec2d7c6ab1d78a596b6ae04a
ggundrum <ggundrum@parchment.com>
2018-07-27 19:12:00 -0700

    adding some chomp to get results from variable.

--- discovery/simple_query.rb
+++ discovery/simple_query.rb
@@ -5,10 +5,10 @@ require 'pg'
 begin
 
   print  "Please enter datbasae username:"
-  user = gets
+  user = gets.chomp
   print "Please entur user password"
-  password = get
-  con = PG.connect :hostaddr => '192.168.1.254', :dbname => 'musiccabinet', :user => "#{username}",
+  password = gets.chomp
+  con = PG.connect :hostaddr => '192.168.1.254', :dbname => 'musiccabinet', :user => "#{user}",
       :password => "#{password}"
 
 
